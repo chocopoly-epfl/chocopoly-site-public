9da413eab4a4d64e8276bdcc4bfeffd4b1f5662e
EdenKahane <kahaneeden@protonmail.com>
2025-01-02 20:36:57 +0100

    Corrected event carcd UI

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -1,36 +1,45 @@
 <!-- @format -->
 <script lang="ts">
-	import ImageB64 from "$lib/components/ImageB64.svelte";
-	import type { EventClient } from "../../app";
+	import ImageB64 from '$lib/components/ImageB64.svelte';
+	import type { EventClient } from '../../app';
+	import Button from './ui/button/button.svelte';
 
-	export let event: EventClient;	
-	
+	export let event: EventClient;
+
 	let regexExpr = /(\n){2,}/g;
 </script>
 
 <div class="event bg-background">
-	<div class="img">
+	<div class="img h-1/2 w-full">
 		{#if event.image}
 			<ImageB64 imageb64={event.image} alt={`Événement ${event.title}`} alternativeImageSrc="" />
 		{/if}
 	</div>
-	<h3>{event.title}</h3>
-	<h5>
-		{new Intl.DateTimeFormat("fr-Fr", {
-			dateStyle: "medium",
-			timeStyle: "short",
-			timeZone: "Europe/Paris",
-		}).format(Date.parse(event.date))}
-	</h5>
-	<p>{event.text ? event.text.replaceAll(regexExpr, "\n") : ""}</p>
-	<a href={"/events/" + event.id} class="button text-secondary">Découvrir</a>
+	<div class="h-1/2">
+		<h3>{event.title}</h3>
+		<h5>
+			{new Intl.DateTimeFormat('fr-Fr', {
+				dateStyle: 'medium',
+				timeStyle: 'short',
+				timeZone: 'Europe/Paris'
+			}).format(Date.parse(event.date))}
+		</h5>
+		<div class="flex flex-col justify-between h-full">
+			<p class="my-2 max-h-2/3 overflow-hidden line-clamp-[11]">{event.text}</p>
+			<Button href={'/events/' + event.id} class="text-secondary w-56 text-xl mx-auto"
+				>Voir l'événement</Button
+			>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
 	.event {
 		padding: 2em 2em 7em 2em;
 		color: black;
-		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+		box-shadow:
+			0 4px 8px 0 rgba(0, 0, 0, 0.2),
+			0 6px 20px 0 rgba(0, 0, 0, 0.19);
 		color: #353535;
 		position: relative;
 		border-radius: 8px;
@@ -43,8 +52,6 @@
 		font-size: small;
 
 		.img {
-			width: 100%;
-			height: 50%;
 			overflow: hidden;
 			border-radius: 8px;
 			position: relative;
@@ -68,41 +75,6 @@
 
 		h5 {
 			margin: 0 0 0 5px;
-		}
-
-		p {
-			margin-top: 4px;
-			color: #827d7d;
-			line-clamp: 8;
-			-webkit-line-clamp: 8;
-			overflow: hidden;
-			display: -webkit-box;
-			-webkit-box-orient: vertical;
-		}
-
-		.button {
-			display: block;
-			width: 12em;
-			text-decoration: none;
-			cursor: pointer;
-			font-size: 1.3em;
-			font-weight: bold;
-			letter-spacing: 2px;
-			line-height: 2.75em;
-			text-align: center;
-			margin: 10px 0;
-			position: absolute;
-			left: 50%;
-			bottom: 10px;
-			transform: translateX(-50%);
-			border-radius: 30px;
-			background: rgb(2, 29, 86);
-			background: linear-gradient(
-				94deg,
-				rgba(2, 29, 86, 1) 0%,
-				rgba(7, 48, 118, 1) 5%,
-				rgba(98, 182, 235, 1) 98%
-			);
 		}
 	}
 </style>
