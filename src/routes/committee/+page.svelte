a34a6ef886c795dbd54eeb9c3b2f9b6d2f164b24
fabnemEPFL <fabrice.nemo@epfl.ch>
2025-02-26 18:22:49 +0100

    description très basique du comité

--- src/routes/committee/+page.svelte
+++ src/routes/committee/+page.svelte
@@ -4,35 +4,102 @@
 
 	const committees: Committee[] = [
 		{
-			name: 'Test Test',
+			name: 'Fabrice Nemo',
 			title: 'Chocoprésident',
-			email: 'president@chocopoly.ch',
+			email: 'fabrice.nemo@epfl.ch',
 			desc: 'Le chocoprésident'
 		},
 		{
-			name: 'Test Test',
-			title: 'Chocoprésident',
-			email: 'president@chocopoly.ch',
-			desc: 'Le chocoprésident'
+			name: 'Laura Paraboschi',
+			title: 'Chocoprésidente',
+			email: 'laura.paraboschi@epfl.ch',
+			desc: 'La chocoprésidente'
 		},
 		{
-			name: 'Test Test',
-			title: 'Chocoprésident',
-			email: 'president@chocopoly.ch',
-			desc: 'Le chocoprésident'
+			name: 'Ryo Saito',
+			title: 'Vice-Président',
+			email: 'contact@chocopoly.ch',
+			desc: 'Le vice-président'
 		},
 		{
-			name: 'Test Test',
-			title: 'Chocoprésident',
-			email: 'president@chocopoly.ch',
-			desc: 'Le chocoprésident'
+			name: 'Camille Constantin',
+			title: 'Trésorière',
+			email: 'contact@chocopoly.ch',
+			desc: 'La trésorière'
 		},
 		{
-			name: 'Test Test',
-			title: 'Chocoprésident',
-			email: 'president@chocopoly.ch',
-			desc: 'Le chocoprésident'
-		}];
+			name: 'Victor Lancieux',
+			title: 'Respo Logistique',
+			email: 'contact@chocopoly.ch',
+			desc: 'Le respo log'
+		},
+		{
+			name: 'Uri Jaquet',
+			title: 'Respo Chocommunication',
+			email: 'contact@chocopoly.ch',
+			desc: 'Le respo chocommunication'
+		},
+		{
+			name: 'Nada Waly',
+			title: 'Graphiste',
+			email: 'contact@chocopoly.ch',
+			desc: 'La graphiste'
+		},
+		{
+			name: 'Sophia Pheng',
+			title: 'Respo R&D',
+			email: 'contact@chocopoly.ch',
+			desc: 'La respo R&D'
+		},
+		{
+			name: 'Antoine Pelletier',
+			title: 'Respo Informatique',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un respo info'
+		},
+		{
+			name: 'Simon Lefort',
+			title: 'Respo Informatique',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un respo info'
+		},
+		{
+			name: 'Alam Khan Burguete',
+			title: 'Respo Informatique',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un respo info'
+		},
+		{
+			name: 'Matthew Meyer',
+			title: 'Staff régulier',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un staff régulier'
+		},
+		{
+			name: 'Adélie Teissier',
+			title: 'Staff régulière',
+			email: 'contact@chocopoly.ch',
+			desc: 'Une staff régulière'
+		},
+		{
+			name: 'Mathieu Zufferey',
+			title: 'Staff régulier',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un staff régulier'
+		},
+		{
+			name: 'Evelyne Sonneville',
+			title: 'Staff régulière',
+			email: 'contact@chocopoly.ch',
+			desc: 'Une staff régulière'
+		},
+		{
+			name: 'Louis Belloir',
+			title: 'Staff régulier',
+			email: 'contact@chocopoly.ch',
+			desc: 'Un staff régulier'
+		}
+		];
 </script>
 
 <div
--- src/routes/committee/CommitteePage.svelte
+++ src/routes/committee/CommitteePage.svelte
@@ -7,7 +7,7 @@
 
 </script>
 
-<div class="bg-white text-center mx-5 my-5 w-64 h-[540px] overflow-y-scroll p-2 relative overflow-x-clip">
+<div class="bg-white text-center mx-5 my-5 w-64 h-[540px] overflow-y-clip p-2 relative overflow-x-clip">
 	
 	{#if imagePath}
 		<img alt={`${name} photo`} src={imagePath} class="rounded-full mx-auto mb-4 w-1/2"/>
@@ -15,7 +15,7 @@
 		<img alt={`empty photo`} src="./committee/default.png" class="rounded-full mx-auto mb-4 w-1/2"/>
 	{/if}
 
-	<h5 class="my-2">{name}</h5>
+	<h5 class="text-xl my-1">{name}</h5>
 	<span class="d-block text-primary my-1" >{title}</span>
 	
 	<p class="text-gray-400 text-sm my-3 w-4/5 mx-auto text-justify">{desc}</p>
